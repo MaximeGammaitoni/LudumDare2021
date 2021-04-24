8a6aef98e97517f1b79ada28cf8bba281b7ab4a9
Maxime Gammaitoni <maxime.gammaitoni@technologypartner.io>
2021-04-24 20:35:09 +0100

    feature/StateManager

---
+++ Assets/Scripts/Events/StatesEvents.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class StatesEvents
+{
+    public StatesEvents()
+    {
+
+    }
+
+    public  UnityAction<Args> OnBeginIn;
+    public  UnityAction<Args> OnBeginOut;
+
+    public  UnityAction<Args> OnRunIn;
+    public  UnityAction<Args> OnRunOut;
+
+    public  UnityAction<Args> OnWinIn;
+    public  UnityAction<Args> OnWinOut;
+
+    public  UnityAction<Args> OnEndIn;
+    public  UnityAction<Args> OnEndOut;
+
+    public UnityAction<Args> OnPauseIn;
+    public UnityAction<Args> OnPauseOut;
+    public class StatesEventArgs : Args
+    {
+        
+    }
+}
---
+++ Assets/Scripts/States/States.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace States
+{
+    public interface IState
+    {
+
+    }
+    public abstract class BaseState : IState
+    {
+        public virtual void In()
+        {
+
+        }
+        public virtual void Out()
+        {
+
+        }
+    }
+
+    public class Begin : BaseState
+    {
+        public override void In()
+        {
+            EventsManager.TriggerEvent("OnBeginIn");
+        }
+        public override void Out()
+        {
+
+            EventsManager.TriggerEvent("OnBeginOut");
+        }
+    }
+    public class Run : BaseState
+    {
+        public override void In()
+        {
+            EventsManager.TriggerEvent("OnRunIn");
+        }
+        public override void Out()
+        {
+
+            EventsManager.TriggerEvent("OnRunOut");
+        }
+    }
+
+    public class Win : BaseState
+    {
+        public override void In()
+        {
+            EventsManager.TriggerEvent("OnWinIn");
+        }
+        public override void Out()
+        {
+
+            EventsManager.TriggerEvent("OnWinOut");
+        }
+    }
+
+    public class End : BaseState
+    {
+        public override void In()
+        {
+            EventsManager.TriggerEvent("OnEndIn");
+        }
+        public override void Out()
+        {
+
+            EventsManager.TriggerEvent("OnEndOut");
+        }
+    }
+
+    public class Pause : BaseState
+    {
+        public override void In()
+        {
+            EventsManager.TriggerEvent("OnPauseIn");
+        }
+        public override void Out()
+        {
+
+            EventsManager.TriggerEvent("OnPauseOut");
+        }
+    }
+
+}
