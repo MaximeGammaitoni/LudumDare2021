2070767740254e8311b42d82196e9b2684bfd39a
josselinMorau <josselin.morau@gmail.com>
2021-04-26 13:00:02 +0200

    [WIP] get and post leaderboard data

--- Assets/Scripts/LeaderBoard/LeaderBoardManager.cs
+++ Assets/Scripts/LeaderBoard/LeaderBoardManager.cs
@@ -29,17 +29,82 @@ public class LeaderBoardManager
     const string BaseUrl = "http://51.91.99.249"; //If you get this url to add your score manually, the Lord of the hell Paimon will come to you cheaty boy.
     const string GetAllEndPoint = "/leader_board/";
 
-
     // Start is called before the first frame update
     public LeaderBoardManager()
     {
         Line = GameManager.singleton.ResourcesLoaderManager.UIElementsLoader.LeaderBoardUiElement;
         LeaderBoardContent = GameManager.singleton.ResourcesLoaderManager.CanvasElements.LeaderBoardContent;
+        EventsManager.StartListening(nameof(EnterName.OnNameEntered), OnNameEntered);
     }
+
     public void GetRequestAndInstantiateIntoCanvas()
     {
         GameManager.singleton.StartCoroutine(GetRequestAndInstantiateIntoCanvasCorout());
     }
+
+    void OnNameEntered(Args args)
+    {
+        if (args is NameArgs nameArgs)
+        {
+            GameManager.singleton.StartCoroutine(OnNameEnteredCoroutine(nameArgs.name));
+        }
+    }
+
+    IEnumerator OnNameEnteredCoroutine(string name)
+    {
+        LeadBoardData data = null;
+        yield return FetchLeaderBoardData(name, res => { data = res; });
+        int currentScore = ScoreManager.GetCurrentScore();
+        if (data == null || currentScore > data.score)
+        {
+            // Post data
+            LeadBoardData newData = new LeadBoardData()
+            {
+                name = name,
+                score = currentScore
+            };
+            yield return PostLeaderBoardData(newData);
+        }
+        GetRequestAndInstantiateIntoCanvas();
+    }
+
+    IEnumerator FetchLeaderBoardData(string name, Action<LeadBoardData> callback)
+    {
+        LeadBoardData data = null;
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(BaseUrl + GetAllEndPoint + name))
+        {
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.result == UnityWebRequest.Result.ConnectionError)
+            {
+                Debug.Log("Error: "  + webRequest.error);
+            }
+            else
+            {
+                try
+                {
+                    data = JsonUtility.FromJson<LeadBoardData>(webRequest.downloadHandler.text);
+                }
+                catch (Exception e)
+                {
+                    Debug.LogError("Couldn't parse JSON correctly : " + e.Message);
+                    data = null;
+                }
+            }
+        }
+        callback?.Invoke(data);
+        yield return null;
+    }
+
+    IEnumerator PostLeaderBoardData(LeadBoardData data)
+    {
+        string json = JsonUtility.ToJson(data);
+        using (UnityWebRequest webRequest = UnityWebRequest.Post(BaseUrl + GetAllEndPoint, json))
+        {
+            yield return webRequest.SendWebRequest();
+        }
+    }
+
     IEnumerator GetRequestAndInstantiateIntoCanvasCorout()
     {
         using (UnityWebRequest webRequest = UnityWebRequest.Get(BaseUrl + GetAllEndPoint))
